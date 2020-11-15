91b20926cceccaa249bb26026c7790d1ecfc345f
HaoChengHao0113 <249911943@qq.com>
2020-11-15 15:17:40 +0800

    'hook'

--- first-app/src/conponments/ReactHooks/useImperativeHandle.js
+++ first-app/src/conponments/ReactHooks/useImperativeHandle.js
@@ -1,8 +1,56 @@
-import React, { useImperativeHandle } from 'react';
+import React, { useImperativeHandle, forwardRef, useRef, useState } from 'react';
+const ForwardRef = forwardRef((props, ref)=>{
+    return(
+        <div>
+            <span ref={ref}>123</span>
+        </div>
+    )
+})
+
+const Imperative =forwardRef((props, refs)=>{
+    const inputRef = useRef();
+    const [count, setCount] = useState(0);
+    const [num, setNum] = useState(0);
+    useImperativeHandle(refs,()=>({
+        name: 'zhangsan',
+        focus(){
+            inputRef.current.focus();
+        },
+        count
+    }),[num])
+    return(
+        <div>
+            <input type="text" ref={inputRef}/>
+            <span>count:{count}</span>
+            <br/>
+            <button onClick={()=>{
+                setCount(count+1)
+            }}>count++</button>
+            <br/>
+            <span>num:{num}</span>
+            <br/>
+            <button onClick={()=>{
+                setNum(num+1)
+            }}>num++</button>
+        </div>
+    )
+})
 export default ()=>{
+    const el = useRef(null) // 获取子组件的dom
+    const Imref = useRef(null);
     return (
         <div>
             <span>useImperativeHandle</span>
+            <ForwardRef ref={el}/>
+            <button onClick={()=>{
+                console.log(el)
+            }}>获取子组件dom</button>
+
+            <Imperative ref={Imref}/>
+            <button onClick={()=>{
+                console.log(Imref.current)
+                Imref.current.focus()
+            }}>获取子组件自定义方法或者属性</button>
             <br/>
             <br/>
             <br/>
